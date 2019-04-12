d68c5431cd599cae5c9ad54f2c98e5d3a826b8a9
Dmitriy <justzofb@gmail.com>
2019-04-12 16:02:05 +0600

    Renames js code on pagePadding

--- js/pagePadding.js
+++ js/pagePadding.js
@@ -3,50 +3,20 @@ let mainPage = $("#main_page");
 let numbers = $("#numbers");
 let contact_us = $("#contact_us");
 
-if ($(window).width() < 560) {
-  topOfPage.removeClass("page__small");
-}
-if ($(window).width() < 560) {
-  mainPage
-    .removeClass("page__large")
-    .addClass("page__medium")
-    .css("padding-bottom", "0px");
-}
-if ($(window).width() < 560) {
-  numbers.removeClass("page__large").addClass("page__medium");
-}
-if ($(window).width() < 560) {
-  contact_us.removeClass("page__medium").addClass("page__small");
-}
+topOfPage.toggleClass("page__small", $(window).width() >= 560);
+mainPage.toggleClass("page__large", $(window).width() >= 560);
+mainPage.toggleClass("page__medium__top", $(window).width() < 560);
+numbers.toggleClass("page__large", $(window).width() >= 560);
+numbers.toggleClass("page__medium", $(window).width() < 560);
+contact_us.toggleClass("page__medium", $(window).width() >= 560);
+contact_us.toggleClass("page__small", $(window).width() < 560);
 
 $(window).resize(function renameClasses() {
-  if (topOfPage.hasClass("page__small") && $(window).width() < 560) {
-    topOfPage.removeClass("page__small");
-  } else if (!topOfPage.hasClass("page__small") && $(window).width() >= 560) {
-    topOfPage.addClass("page__small");
-  }
-
-  if (mainPage.hasClass("page__large") && $(window).width() < 560) {
-    mainPage
-      .removeClass("page__large")
-      .addClass("page__medium")
-      .css("padding-bottom", "0px");
-  } else if (!mainPage.hasClass("page__large") && $(window).width() >= 560) {
-    mainPage
-      .removeClass("page__medium")
-      .addClass("page__large")
-      .css("padding-bottom", "unset");
-  }
-
-  if (numbers.hasClass("page__large") && $(window).width() < 560) {
-    numbers.removeClass("page__large").addClass("page__medium");
-  } else if (!numbers.hasClass("page__large") && $(window).width() >= 560) {
-    numbers.removeClass("page__medium").addClass("page__large");
-  }
-
-  if (contact_us.hasClass("page__medium") && $(window).width() < 560) {
-    contact_us.removeClass("page__medium").addClass("page__small");
-  } else if (!contact_us.hasClass("page__medium") && $(window).width() >= 560) {
-    contact_us.removeClass("page__small").addClass("page__medium");
-  }
+  topOfPage.toggleClass("page__small", $(window).width() >= 560);
+  mainPage.toggleClass("page__large", $(window).width() >= 560);
+  mainPage.toggleClass("page__medium__top", $(window).width() < 560);
+  numbers.toggleClass("page__large", $(window).width() >= 560);
+  numbers.toggleClass("page__medium", $(window).width() < 560);
+  contact_us.toggleClass("page__medium", $(window).width() >= 560);
+  contact_us.toggleClass("page__small", $(window).width() < 560);
 });
